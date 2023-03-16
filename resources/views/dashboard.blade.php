83d68e5e5ad18033802e3d98070940ebe0f7d4bd
06066060606060 <xmicky+git@hotmail.fr>
2023-03-16 18:05:55 +0400

    upd

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
                     <div class="flex flex-wrap items-center justify-between pb-4 mx-2 mr-2 border-b border-gray-700 lg:mx-16">
                         <a href="#" class="flex items-center">
                            <i class="mr-3 fa-regular fa-map"></i>
-                            <span class="self-center text-xl font-semibold text-white whitespace-nowrap">City Layer</span>
+                            <span class="self-center text-xl font-semibold text-white select-none whitespace-nowrap">City Layer</span>
                         </a>
                         <button data-collapse-toggle="navbar-multi-level" type="button"
                             class="inline-flex items-center p-2 ml-3 text-sm text-gray-400 rounded-lg focus:outline-none focus:ring-2 hover:bg-gray-700 focus:ring-gray-600"
@@ -20,13 +20,7 @@
                             </svg>
                         </button>
                         <div class="relative hidden w-full" id="navbar-multi-level">
-                            <ul
-                                class="flex flex-col p-4 mt-4 bg-gray-800 border border-gray-700 rounded-lg">
-                                <li>
-                                    <a href="#"
-                                        class="block py-2 pl-3 pr-4 text-white bg-blue-600 rounded"
-                                        aria-current="page">Home</a>
-                                </li>
+                            <ul class="flex flex-col p-4 mt-4 bg-gray-800 border border-gray-700 rounded-lg">
                                 <li>
                                     <a href="#"
                                         class="block py-2 pl-3 pr-4 text-gray-400 rounded hover:bg-gray-700 hover:text-white">Profile</a>
@@ -56,23 +50,23 @@
 
 
             <div class="z-0 p-3 pt-16 space-y-4 lg:mx-16 md:pt-20">
-                <h4 class="pt-4 mt-2 font-semibold">Start Mapping</h4>
+                <h4 class="pt-4 mt-2 font-semibold select-none">Start Mapping</h4>
                 <div class="flex items-center justify-between space-x-3 overflow-y-scroll">
                     <div
                         class="flex flex-col items-center justify-center w-20 h-20 p-1 mb-2 text-gray-800 transition duration-300 ease-in bg-green-200 shadow cursor-pointer hover:bg-green-300 active:bg-green-400 rounded-2xl hover:shadow-md">
                         <i class="fa-solid fa-road"></i>
-                        <p class="mt-1 text-xs">Street</p>
+                        <p class="mt-1 text-xs select-none">Street</p>
                     </div>
                     <div
                         class="flex flex-col items-center justify-center w-20 h-20 p-1 mb-2 text-gray-800 transition duration-300 ease-in bg-yellow-200 shadow cursor-pointer hover:bg-yellow-300 active:bg-yellow-400 rounded-2xl hover:shadow-md">
                         <i class="fa-solid fa-building"></i>
-                        <p class="mt-1 text-xs">Building</p>
+                        <p class="mt-1 text-xs select-none">Building</p>
                     </div>
 
                     <div
                         class="flex flex-col items-center justify-center w-20 h-20 p-1 mb-2 text-gray-800 transition duration-300 ease-in bg-indigo-200 shadow cursor-pointer hover:bg-indigo-300 active:bg-indigo-400 rounded-2xl hover:shadow-md">
                         <i class="fa-solid fa-street-view"></i>
-                        <p class="mt-1 text-xs">Open space</p>
+                        <p class="mt-1 text-xs select-none">Open space</p>
                     </div>
                 </div>
 
@@ -81,7 +75,7 @@
                     </div>
                 </div>
 
-                <h4 class="font-semibold">Your data</h4>
+                <h4 class="font-semibold select-none">Your data</h4>
                 <div class="grid grid-cols-1">
                     <div class="">
                         <div class="flex p-2 mb-2 bg-white shadow-md rounded-2xl">
@@ -90,7 +84,7 @@
                             <div class="flex flex-col justify-center w-full px-2 py-1">
                                 <div class="flex items-center justify-between ">
                                     <div class="flex flex-col">
-                                        <h2 class="text-sm font-medium text-gray-800">Massive Dynamic</h2>
+                                        <h2 class="text-sm font-medium text-gray-800 select-none">Dynamic street</h2>
                                     </div>
                                     <svg xmlns="http://www.w3.org/2000/svg"
                                         class="w-5 h-5 text-gray-500 cursor-pointer hover:text-blue-400" fill="none"
@@ -107,9 +101,9 @@
                                                 d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
                                             </path>
                                         </svg>
-                                        <p class="font-normal">4.5</p>
+                                        <p class="font-normal"></p>
                                     </div>
-                                    <div class="flex items-center font-medium text-gray-900 ">
+                                    <div class="flex items-center font-medium text-gray-900 select-none">
                                         Paris
                                     </div>
                                 </div>
@@ -122,7 +116,7 @@
                             <div class="flex flex-col justify-center w-full px-2 py-1">
                                 <div class="flex items-center justify-between ">
                                     <div class="flex flex-col">
-                                        <h2 class="text-sm font-medium text-gray-800">Massive Dynamic</h2>
+                                        <h2 class="text-sm font-medium text-gray-800 select-none">Test building</h2>
                                     </div>
                                     <svg xmlns="http://www.w3.org/2000/svg"
                                         class="w-5 h-5 text-gray-500 cursor-pointer hover:text-blue-400" fill="none"
@@ -139,9 +133,9 @@
                                                 d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
                                             </path>
                                         </svg>
-                                        <p class="font-normal">4.5</p>
+                                        <p class="font-normal"></p>
                                     </div>
-                                    <div class="flex items-center font-medium text-gray-900 ">
+                                    <div class="flex items-center font-medium text-gray-900 select-none">
                                         Paris
                                     </div>
                                 </div>
