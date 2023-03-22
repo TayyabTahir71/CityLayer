f1f12f9a33e4787846cf0e6915b391e30dc0a169
06066060606060 <xmicky+git@hotmail.fr>
2023-03-22 19:53:51 +0400

    fixx

--- resources/views/step2.blade.php
+++ resources/views/step2.blade.php
@@ -7,8 +7,8 @@
             <div class="pt-16 md:pt-20">
                 <div id="map" class="mt-4 h-[50vh] lg:h-[70vh] w-auto z-0"></div>
                 <div x-data="{ modelOpen: true }">
-                    <div x-cloak x-show="modelOpen" class="fixed bottom-0 overflow-y-auto w-screen" aria-labelledby="modal-title"
-                        role="dialog" aria-modal="true">
+                    <div x-cloak x-show="modelOpen" class="fixed bottom-0 overflow-y-auto w-screen"
+                        aria-labelledby="modal-title" role="dialog" aria-modal="true">
                         <div class="flex justify-center text-center items-end">
                             <div x-cloak x-show="modelOpen" x-transition:enter="transition ease-out duration-300 transform"
                                 x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
@@ -19,36 +19,37 @@
                                 class="flex justify-center w-screen p-8 overflow-hidden text-left transition-all transform bg-[#CDB8EB] shadow-xl z-50">
 
                                 <div class="items-center space-x-4 max-w-2xl">
-                                    <h1 class="text-2xl font-bold text-center text-black">How do you feel in this space?</h1>
+                                    <h1 class="text-2xl font-bold text-center text-black">How do you feel in this space?
+                                    </h1>
                                     <div class="flex flex-col pt-4 space-y-6">
                                         <div class="flex justify-between w-full">
-                                                <button class="">
-                                                    <img src="/img/happy.png" alt="happy" class="w-16 h-16 mb-2"><h1 class=" font-bold">happy</h1>
-                                                </button>
-                                        
-                                                <button class="">
-                                                    <img src="/img/sad.png" alt="sad" class="w-16 h-16 mb-2"><h1 class=" font-bold">sad</h1>
-                                                </button>
+                                            <button class="" @click="modelOpen = false"
+                                                onclick="openModal('main-modal')">
+                                                <img src="/img/happy.png" alt="happy" class="w-16 h-16 mb-2">
+                                                <h1 class=" font-bold">happy</h1>
+                                            </button>
+                                            <button class="" @click="modelOpen = false">
+                                                <img src="/img/sad.png" alt="sad" class="w-16 h-16 mb-2">
+                                                <h1 class=" font-bold">sad</h1>
+                                            </button>
+                                            <button class="" @click="modelOpen = false">
+                                                <img src="/img/stressed.png" alt="stressed" class="w-16 h-16 mb-2">
+                                                <h1 class=" font-bold">stressed</h1>
+                                            </button>
 
-                                                <button class="">
-                                                    <img src="/img/stressed.png" alt="stressed" class="w-16 h-16 mb-2"><h1 class=" font-bold">stressed</h1>
-                                                </button>
-                                         
                                         </div>
-                                         <div class="flex justify-between w-full">
-                                         
-                                                <button class="">
-                                                    <img src="/img/angry.png" alt="angry" class="w-16 h-16 mb-2"><h1 class=" font-bold">angry</h1>
-                                                </button>
-                                        
-                                                <button class="">
-                                                    <img src="/img/worried.png" alt="worried" class="w-16 h-16 mb-2"><h1 class=" font-bold">worried</h1>
-                                                </button>
-
-                                                <button class="mr-2">
-                                                    <h1 class="pt-4 font-bold">/choose<br>other</h1>
-                                                </button>
-                                         
+                                        <div class="flex justify-between w-full">
+                                            <button class="" @click="modelOpen = false">
+                                                <img src="/img/angry.png" alt="angry" class="w-16 h-16 mb-2">
+                                                <h1 class=" font-bold">angry</h1>
+                                            </button>
+                                            <button class="" @click="modelOpen = false">
+                                                <img src="/img/worried.png" alt="worried" class="w-16 h-16 mb-2">
+                                                <h1 class=" font-bold">worried</h1>
+                                            </button>
+                                            <button class="mr-2" @click="modelOpen = false">
+                                                <h1 class="pt-4 font-bold">/choose<br>other</h1>
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
@@ -56,14 +57,35 @@
                         </div>
                     </div>
                 </div>
-
+                <div
+                    class="main-modal">
+                    <div class="modal-container">
+                        <div class="modal-content">
+                            <div class="fixed bottom-0 overflow-y-auto w-screen">
+                                    <div
+                                        class="flex justify-center w-screen p-8 overflow-hidden text-left transition-all transform bg-[#CDB8EB] shadow-xl z-50">
+                                        <div class="items-center space-x-4 max-w-2xl">
+                                            <h1 class="text-3xl text-center text-black pb-8">Share what makes you feel that way!
+                                            </h1>
+                                            <div class="flex flex-col pt-4 space-y-6">
+                                                <div class="flex justify-center w-full">
+                                           <button class="" onclick="closeModal('main-modal')">
+                                                <h1 class="text-3xl text-center font-bold bg-white rounded-xl py-8">Describe and share a photo!</h1>
+                                            </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                           
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-       
     </div>
 
     <script>
-      
         markers = {};
         let marker = null;
         let mymap0 = L.map('map').setView([48.6890, 7.14086], 15);
@@ -86,16 +108,15 @@
 
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function(position) {
-                mymap0.setView([position.coords.latitude, position.coords.longitude], 19);
-                L.marker([position.coords.latitude, position.coords.longitude], {
-                    icon: icon
-                }).addTo(mymap0);
-            },
-            function(e) {
-        }, {
-            enableHighAccuracy: true
-        });
-        } 
+                    mymap0.setView([position.coords.latitude, position.coords.longitude], 19);
+                    L.marker([position.coords.latitude, position.coords.longitude], {
+                        icon: icon
+                    }).addTo(mymap0);
+                },
+                function(e) {}, {
+                    enableHighAccuracy: true
+                });
+        }
 
 
         $.ajaxSetup({
@@ -155,7 +176,30 @@
                     }
                 }
             });
-         
+
+        }
+
+        all_modals = ['main-modal']
+        all_modals.forEach((modal) => {
+            const modalSelected = document.querySelector('.' + modal);
+            modalSelected.classList.remove('fadeIn');
+            modalSelected.classList.add('fadeOut');
+            modalSelected.style.display = 'none';
+        })
+        const modalClose = (modal) => {
+            const modalToClose = document.querySelector('.' + modal);
+            modalToClose.classList.remove('fadeIn');
+            modalToClose.classList.add('fadeOut');
+            setTimeout(() => {
+                modalToClose.style.display = 'none';
+            }, 500);
+        }
+
+        const openModal = (modal) => {
+            const modalToOpen = document.querySelector('.' + modal);
+            modalToOpen.classList.remove('fadeOut');
+            modalToOpen.classList.add('fadeIn');
+            modalToOpen.style.display = 'flex';
         }
     </script>
     <style>
