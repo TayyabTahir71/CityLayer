0967fbd6a5fad45a574ef278a5b8b5cd4020a10e
06066060606060 <xmicky+git@hotmail.fr>
2023-04-25 12:29:37 +0200

    alert

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -120,7 +120,7 @@
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(showPosition, showError);
         } else {
-            x.innerHTML = "Geolocation is not supported by this browser.";
+            alert("Geolocation is not supported by this browser.");
         }
 
         function showPosition(position) {
@@ -133,16 +133,16 @@
         function showError(error) {
             switch (error.code) {
                 case error.PERMISSION_DENIED:
-                    x.innerHTML = "User denied the request for Geolocation."
+                    alert("User denied the request for Geolocation.");
                     break;
                 case error.POSITION_UNAVAILABLE:
-                    x.innerHTML = "Location information is unavailable."
+                    alert("Location information is unavailable.");
                     break;
                 case error.TIMEOUT:
-                    x.innerHTML = "The request to get user location timed out."
+                     alert("The request to get user location timed out.");
                     break;
                 case error.UNKNOWN_ERROR:
-                    x.innerHTML = "An unknown error occurred."
+                     alert("An unknown error occurred.");
                     break;
             }
         }
