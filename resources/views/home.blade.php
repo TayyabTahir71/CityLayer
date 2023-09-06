08e67af78ffd438d5ce3cbda8cfb25e3392d9053
Muhammad Bilal <mb.bilal997@gmail.com>
2023-09-06 11:57:12 +0500

    homepage in progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,72 +11,84 @@
         <div class="flex flex-col mx-auto">
             <div class="">
                 <div class="relative">
+
+                    <div class="absolute top-8 left-4 p-2 rounded-lg bg-black z-20">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-8 h-8 text-white">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
+                    </div>
+
                     <div id="map" class="absolute w-[100vw] z-10 h-[90vh]"></div>
                 </div>
                 <div x-data="{ modelOpen: false }">
-                    <div
-                        class="fixed bottom-0 w-full py-6 text-2xl font-bold text-black bg-[#B8E7EB] active:bg-blue-400 text-center z-50">
-                        <a class="mx-12" @click="modelOpen =!modelOpen" onclick="zoom()">
-                            {{ __('messages.Start Playing!') }}</a>
+                    <div class="fixed bottom-0 w-full py-3 text-2xl font-bold bg-black  text-center z-50">
+                        <a class="mx-12" @click="modelOpen =!modelOpen">
+                            <div class="bg-white py-2 rounded-3xl text-black">
+                                {{ __('messages.This place!') }}
 
+                        </a>
                     </div>
 
 
-                    <div x-cloak x-show="modelOpen" class="fixed bottom-0 z-50 overflow-y-auto"
-                        aria-labelledby="modal-title" role="dialog" aria-modal="true">
-                        <div class="flex items-end justify-center text-center">
-                            <div x-cloak @click="modelOpen = false" x-show="modelOpen"
-                                x-transition:enter="transition ease-out duration-300 transform"
-                                x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
-                                x-transition:leave="transition ease-in duration-200 transform"
-                                x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
-                                class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-40" aria-hidden="true">
-                            </div>
+                </div>
 
-                            <div x-cloak x-show="modelOpen" x-transition:enter="transition ease-out duration-300 transform"
-                                x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                                x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
-                                x-transition:leave="transition ease-in duration-200 transform"
-                                x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-                                x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                                class="inline-block w-screen p-8 mt-60 overflow-hidden text-left transition-all transform bg-[#B8E7EB] shadow-xl z-50">
 
-                                <div class="items-center space-x-4 bloc">
+                <div x-cloak x-show="modelOpen" class="fixed bottom-0 z-50 overflow-y-auto" aria-labelledby="modal-title"
+                    role="dialog" aria-modal="true">
+                    <div class="flex items-end justify-center text-center">
+                        <div x-cloak @click="modelOpen = false" x-show="modelOpen"
+                            x-transition:enter="transition ease-out duration-300 transform"
+                            x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
+                            x-transition:leave="transition ease-in duration-200 transform"
+                            x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
+                            class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-40" aria-hidden="true">
+                        </div>
 
-                                    <h1 class="text-3xl font-bold text-center text-black">
-                                        {{ __('messages.This space is...') }}</h1>
-                                    <div class="flex flex-col pt-6">
-                                        <div class="flex justify-center">
+                        <div x-cloak x-show="modelOpen" x-transition:enter="transition ease-out duration-300 transform"
+                            x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+                            x-transition:leave="transition ease-in duration-200 transform"
+                            x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
+                            x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            class="inline-block w-screen p-8 mt-60 overflow-hidden text-left transition-all transform shadow-xl z-50">
 
-                                            <a href="street"> <button
-                                                    class="w-32 h-32 mx-4 py-6 text-gray-100 bg-[#55C5CF] hover:bg-blue-400  focus:outline-none hover:text-gray-200 rounded-full">
-                                                    <i
-                                                        class="fa-solid fa-road"></i><br>{{ __('messages.a street') }}</button>
-                                            </a>
-                                        </div>
-                                        <div class="flex justify-center space-x-6">
-                                            <a href="building">
-                                                <button
-                                                    class="w-32 h-32 py-6 text-gray-100 bg-[#55C5CF] hover:bg-blue-400 rounded-full focus:outline-none hover:text-gray-200">
-                                                    <i
-                                                        class="fa-solid fa-building"></i><br>{{ __('messages.a building') }}</button>
-                                            </a>
-                                            <a href="openspace">
-                                                <button
-                                                    class="w-32 h-32  py-6 text-gray-100 bg-[#55C5CF] hover:bg-blue-400 rounded-full focus:outline-none hover:text-gray-200">
-                                                    <i
-                                                        class="fa-solid fa-street-view"></i><br>{{ __('messages.an open space') }}</button>
-                                            </a>
-                                        </div>
+                            <div class="items-center space-x-4 block">
+
+                                <h1 class="text-3xl font-bold text-center">
+                                    {{ __('messages.This space is...') }}</h1>
+                                <div class="flex flex-col pt-6">
+                                    <div class="flex justify-center">
+
+                                        <a href="street"> <button
+                                                class="w-32 h-32 mx-4 py-6 text-gray-100 bg-[#55C5CF] hover:bg-blue-400  focus:outline-none hover:text-gray-200 rounded-full">
+                                                <i class="fa-solid fa-road"></i><br>{{ __('messages.a street') }}</button>
+                                        </a>
+                                    </div>
+                                    <div class="flex justify-center space-x-6">
+                                        <a href="building">
+                                            <button
+                                                class="w-32 h-32 py-6 text-gray-100 bg-[#55C5CF] hover:bg-blue-400 rounded-full focus:outline-none hover:text-gray-200">
+                                                <i
+                                                    class="fa-solid fa-building"></i><br>{{ __('messages.a building') }}</button>
+                                        </a>
+                                        <a href="openspace">
+                                            <button
+                                                class="w-32 h-32  py-6 text-gray-100 bg-[#55C5CF] hover:bg-blue-400 rounded-full focus:outline-none hover:text-gray-200">
+                                                <i
+                                                    class="fa-solid fa-street-view"></i><br>{{ __('messages.an open space') }}</button>
+                                        </a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
+
         </div>
+    </div>
     </div>
 
     <script>
@@ -93,6 +105,7 @@
             maxZoom: 19,
             ext: 'png'
         }).addTo(mymap0);
+        mymap0.zoomControl.remove();
         mymap0.addLayer(osmLayer0);
         mymap0.touchZoom.enable();
         mymap0.scrollWheelZoom.enable();
@@ -124,62 +137,67 @@
         }
 
 
-      /*  function getposition() {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    mymap0.setView([position.coords.latitude, position.coords.longitude], 10);
-                    L.marker([position.coords.latitude, position.coords.longitude], {
-                        icon: icon
-                    }).addTo(mymap0);
-                },
-                (e) => {
-                   $.getJSON('https://ipinfo.io/geo', function(response) {
-        var loc = response.loc.split(',');
-        var coords = {
-            latitude: loc[0],
-            longitude: loc[1]
-        };
-         mymap0.setView([coords.latitude, coords.longitude], 10);
-                    L.marker([coords.latitude, coords.longitude], {
-                        icon: icon
-                    }).addTo(mymap0);
-        });
-                }, {
-                    enableHighAccuracy: true,
-                }
-            );
-        }
-        */
+        /*  function getposition() {
+              navigator.geolocation.getCurrentPosition(
+                  (position) => {
+                      mymap0.setView([position.coords.latitude, position.coords.longitude], 10);
+                      L.marker([position.coords.latitude, position.coords.longitude], {
+                          icon: icon
+                      }).addTo(mymap0);
+                  },
+                  (e) => {
+                     $.getJSON('https://ipinfo.io/geo', function(response) {
+          var loc = response.loc.split(',');
+          var coords = {
+              latitude: loc[0],
+              longitude: loc[1]
+          };
+           mymap0.setView([coords.latitude, coords.longitude], 10);
+                      L.marker([coords.latitude, coords.longitude], {
+                          icon: icon
+                      }).addTo(mymap0);
+          });
+                  }, {
+                      enableHighAccuracy: true,
+                  }
+              );
+          }
+          */
 
- function getposition(success, fail){
-    var is_echo = false;
-    if(navigator && navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        function(pos) {
-          if (is_echo){ return; }
-          is_echo = true;
-          success(pos.coords.latitude,pos.coords.longitude);
-        },
-        function() {
-          if (is_echo){ return; }
-          is_echo = true;
-          fail();
+        function getposition(success, fail) {
+            var is_echo = false;
+            if (navigator && navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    function(pos) {
+                        if (is_echo) {
+                            return;
+                        }
+                        is_echo = true;
+                        success(pos.coords.latitude, pos.coords.longitude);
+                    },
+                    function() {
+                        if (is_echo) {
+                            return;
+                        }
+                        is_echo = true;
+                        fail();
+                    }
+                );
+            } else {
+                fail();
+            }
         }
-      );
-    } else {
-      fail();
-    }
-  }
 
-  function success(lat, lng){
-      mymap0.setView([lat, lng], 10);
-                    L.marker([lat, lng], {
-                        icon: icon
-                    }).addTo(mymap0);
-  }
-  function fail(){
-    alert("location failed");
-  }
+        function success(lat, lng) {
+            mymap0.setView([lat, lng], 10);
+            L.marker([lat, lng], {
+                icon: icon
+            }).addTo(mymap0);
+        }
+
+        function fail() {
+            alert("location failed");
+        }
 
 
 
@@ -236,74 +254,82 @@
         }
 
         function mylocation() {
-                  getmyposition(success, fail);
+            getmyposition(success, fail);
         }
 
- function getmyposition(success, fail){
-    var is_echo = false;
-    if(navigator && navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        function(pos) {
-          if (is_echo){ return; }
-          is_echo = true;
-          success(pos.coords.latitude,pos.coords.longitude);
-        },
-        function() {
-          if (is_echo){ return; }
-          is_echo = true;
-          fail();
+        function getmyposition(success, fail) {
+            var is_echo = false;
+            if (navigator && navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    function(pos) {
+                        if (is_echo) {
+                            return;
+                        }
+                        is_echo = true;
+                        success(pos.coords.latitude, pos.coords.longitude);
+                    },
+                    function() {
+                        if (is_echo) {
+                            return;
+                        }
+                        is_echo = true;
+                        fail();
+                    }
+                );
+            } else {
+                fail();
+            }
         }
-      );
-    } else {
-      fail();
-    }
-  }
 
-  function success(lat, lng){
-     mymap0.flyTo([lat, lng], 19);
-       L.marker([lat, lng], {
-                        icon: icon
-                    }).addTo(mymap0);
-  }
-  function fail(){
-    alert("location failed");
-  }
+        function success(lat, lng) {
+            mymap0.flyTo([lat, lng], 19);
+            L.marker([lat, lng], {
+                icon: icon
+            }).addTo(mymap0);
+        }
+
+        function fail() {
+            alert("location failed");
+        }
 
         function zoom() {
             myzoom(success, fail);
         }
 
- function myzoom(success, fail){
-    var is_echo = false;
-    if(navigator && navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        function(pos) {
-          if (is_echo){ return; }
-          is_echo = true;
-          success(pos.coords.latitude,pos.coords.longitude);
-        },
-        function() {
-          if (is_echo){ return; }
-          is_echo = true;
-          fail();
+        function myzoom(success, fail) {
+            var is_echo = false;
+            if (navigator && navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    function(pos) {
+                        if (is_echo) {
+                            return;
+                        }
+                        is_echo = true;
+                        success(pos.coords.latitude, pos.coords.longitude);
+                    },
+                    function() {
+                        if (is_echo) {
+                            return;
+                        }
+                        is_echo = true;
+                        fail();
+                    }
+                );
+            } else {
+                fail();
+            }
         }
-      );
-    } else {
-      fail();
-    }
-  }
 
-  function success(lat, lng){
-     mymap0.flyTo([lat, lng], 16);
-       L.marker([lat, lng], {
-                        icon: icon
-                    }).addTo(mymap0);
-  }
-  function fail(){
-    alert("location failed");
-  }
+        function success(lat, lng) {
+            mymap0.flyTo([lat, lng], 16);
+            L.marker([lat, lng], {
+                icon: icon
+            }).addTo(mymap0);
+        }
 
-
+        function fail() {
+            alert("location failed");
+        }
     </script>
     <style>
 
