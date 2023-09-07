9285255c6813579cdd8ef7fb906c35ae2e70c759
Muhammad Bilal <mb.bilal997@gmail.com>
2023-09-07 20:21:43 +0500

    home modal done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,21 +20,33 @@
                         </svg>
                     </div>
 
+                    <div class="absolute top-[80vh] left-4 p-4 flex justify-center items-center rounded-full bg-black z-20">
+                        <img src="{{ asset('img/triangle.png') }}" class="w-7 h-7" alt="">
+                    </div>
+
                     <div id="map" class="absolute w-[100vw] z-10 h-[90vh]"></div>
                 </div>
                 <div x-data="{ modelOpen: false }">
                     <div
-                        class="fixed bottom-0 w-full py-8 text-xl font-semibold rounded-t-3xl bg-gray-50  shadow-xl  text-center z-50 ">
+                        class="fixed bottom-0 w-full pb-8 pt-3 text-xl font-semibold rounded-t-3xl bg-gray-50  shadow-xl  text-center z-50 ">
+
+                        <div class="flex justify-center items-center">
+                            <div class="py-0.5 w-14 mb-7 rounded-full px-4 bg-black">
+
+                            </div>
+
+                        </div>
+
 
                         <div class="flex">
-                            <div class="absolute left-16 bottom-[18px] ">
+                            <div class="absolute left-16 bottom-[11px] ">
                                 <div class="p-3 rounded-full bg-yellow-500 border-2 border-black">
                                     <img src="{{ asset('img/search-icon.png') }}" class="w-7 h-7" alt="">
                                 </div>
 
                             </div>
-                            <div class="absolute left-6 bottom-[18px] ">
-                                <div class="p-3 rounded-full bg-blue-500 border-2 border-black">
+                            <div class="absolute left-6 bottom-[11px] ">
+                                <div class="p-3 rounded-full bg-cyan-500 border-2 border-black">
                                     <img src="{{ asset('img/image.png') }}" class="w-7 h-7" alt="">
                                 </div>
 
@@ -42,14 +54,14 @@
                         </div>
 
                         <a class="w-full pt-4" @click="modelOpen =!modelOpen">
-                            <span class="bg-blue-500 py-4 rounded-3xl text-white w-full px-12">
+                            <span class="bg-cyan-500 py-4 rounded-3xl text-white w-full px-12">
                                 Add on map
                             </span>
 
                         </a>
 
                         <div class="absolute right-5 bottom-[25px] ">
-                            <div class="p-1 rounded-full bg-blue-500 w-9">
+                            <div class="p-1 rounded-full bg-cyan-500 w-9">
                                 <span class="italic  text-white font-bold">
                                     i
                                 </span>
@@ -76,24 +88,24 @@
                                 x-transition:leave="transition ease-in duration-200 transform"
                                 x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
                                 x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                                class="inline-block w-screen p-4 mt-60 overflow-hidden text-left bg-black rounded-t-3xl transition-all transform shadow-xl z-50">
+                                class="inline-block w-screen p-4 mt-60 overflow-hidden text-left bg-white rounded-t-3xl transition-all transform shadow-xl z-50">
 
-                                <div class="px-1 pt-4" x-data="{ tab: 'feature' }">
+                                <div class="px-1 pt-4" x-data="{ tab: 'place' }">
 
                                     <input type="text" class="rounded-full px-2 w-full bg-white py-2"
                                         placeholder="Choose tags or add new city layers" name="input" id="">
 
 
                                     <div class="flex mt-20 justify-center items-center">
-                                        <div class="absolute left-[48%] top-[90px] cursor-pointer" @click="tab='place'"
-                                            onclick="place()">
+                                        <div class="absolute left-[48%] top-[90px] cursor-pointer" @click="tab='feature'"
+                                            onclick="feature()">
                                             <div class="flex flex-col">
 
-                                                <div class="rounded-full  border-2 border-black"
-                                                    :class="tab == 'place' ? 'bg-yellow-500 p-4' :
-                                                        'bg-yellow-500/70 p-[30px]'">
+                                                <div class="rounded-full shadow-xl  border-2 border-black"
+                                                    :class="tab == 'feature' ? 'bg-yellow-500 p-[22px]' :
+                                                        'bg-yellow-500/70 p-[35px]'">
                                                     <img src="{{ asset('img/search-icon.png') }}" class="w-7 h-7"
-                                                        :class="tab == 'place' ? 'block' : 'hidden'" id="place"
+                                                        :class="tab == 'feature' ? 'block' : 'hidden'" id="feature"
                                                         alt="">
                                                 </div>
 
@@ -102,13 +114,14 @@
                                             </div>
 
                                         </div>
-                                        <div class="absolute left-[37%] top-[90px] cursor-pointer" @click="tab='feature'"
-                                            onclick="feature()">
+                                        <div class="absolute left-[37%] top-[90px] cursor-pointer" @click="tab='place'"
+                                            onclick="place()">
                                             <div class="flex flex-col">
-                                                <div class="rounded-full bg-blue-500 border-2 border-black"
-                                                    :class="tab == 'feature' ? 'bg-blue-500 p-4' : 'bg-blue-500/70 p-[30px]'">
+                                                <div class="rounded-full shadow-xl bg-cyan-500 border-2 border-black"
+                                                    :class="tab == 'place' ? 'bg-cyan-500 p-[22px]' :
+                                                        'bg-cyan-500/70 p-[35px]'">
                                                     <img src="{{ asset('img/image.png') }}" class="w-7 h-7"
-                                                        :class="tab == 'feature' ? 'block' : 'hidden'" id="feature"
+                                                        :class="tab == 'place' ? 'block' : 'hidden'" id="place"
                                                         alt="">
                                                 </div>
 
@@ -120,20 +133,20 @@
 
                                     </div>
 
-                                    <div class="flex mt-44 justify-center items-center">
-                                        <div class="absolute left-[52%] top-[150px] cursor-pointer" @click="tab='place'"
+                                    <div class="flex mt-44 justify-center items-center italic">
+                                        <div class="absolute left-[52%] top-[170px] cursor-pointer" @click="tab='feature'"
                                             onclick="place()">
 
 
-                                            <div class="w-8" :class="tab == 'place' ? 'text-white' : 'text-white/30'">
+                                            <div class="w-8" :class="tab == 'feature' ? 'text-black' : 'text-black/50'">
                                                 Browse Observation
                                             </div>
 
                                         </div>
-                                        <div class="absolute left-[37%] top-[150px] cursor-pointer" @click="tab='feature'"
+                                        <div class="absolute left-[37%] top-[170px] cursor-pointer" @click="tab='place'"
                                             onclick="feature()">
 
-                                            <div class="w-8" :class="tab == 'feature' ? 'text-white' : 'text-white/30'">
+                                            <div class="w-8" :class="tab == 'place' ? 'text-black' : 'text-black/50'">
                                                 Browse Places
                                             </div>
 
@@ -147,19 +160,87 @@
 
 
 
-                                    <div class="flex absolute top-[200px] gap-4">
-                                        <div class="rounded-full bg-blue-500 border-2 border-black p-[30px]">
+                                    <div class="flex absolute left-16 right-16 top-[230px] gap-3 italic"
+                                        x-show="tab=='place'">
+                                        <div class="flex justify-center items-center flex-col">
+                                            <div class="rounded-full bg-cyan-500 border-2  border-black p-[35px]">
 
+                                            </div>
+                                            <span class="text-black mt-2">Place 1</span>
                                         </div>
-                                        <div class="rounded-full bg-blue-500 border-2 border-black p-[30px]">
 
+                                        <div class="flex justify-center items-center flex-col">
+                                            <div class="rounded-full bg-cyan-500 border-2  border-black p-[35px]">
+
+                                            </div>
+                                            <span class="text-black mt-2">Place 2</span>
                                         </div>
+
+                                        <div class="flex justify-center items-center flex-col">
+                                            <div class="rounded-full border-cyan-500 border-2  p-[22px]">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                    viewBox="0 0 24 24" stroke-width="3" stroke="currentColor"
+                                                    class="w-6 h-6 font-bold text-cyan-500">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M12 6v12m6-6H6" />
+                                                </svg>
+
+                                            </div>
+                                            <span class="text-black mt-2">See more</span>
+                                        </div>
+
+
+
+
+
+                                    </div>
+                                    <div class="flex absolute left-16 right-16 top-[230px] gap-3 italic"
+                                        x-show="tab=='feature'">
+                                        <div class="flex justify-center items-center flex-col">
+                                            <div class="rounded-full bg-yellow-500 border-2  border-black p-[35px]">
+
+                                            </div>
+                                            <span class="text-black mt-2">Feature 1</span>
+                                        </div>
+
+                                        <div class="flex justify-center items-center flex-col">
+                                            <div class="rounded-full bg-yellow-500 border-2  border-black p-[35px]">
+
+                                            </div>
+                                            <span class="text-black mt-2">Feature 2</span>
+                                        </div>
+
+                                        <div class="flex justify-center items-center flex-col">
+                                            <div class="rounded-full border-yellow-500 border-2  p-[22px]">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                    viewBox="0 0 24 24" stroke-width="3" stroke="currentColor"
+                                                    class="w-6 h-6 font-bold text-yellow-500">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M12 6v12m6-6H6" />
+                                                </svg>
+
+                                            </div>
+                                            <span class="text-black mt-2">See more</span>
+                                        </div>
+
+
+
+
 
                                     </div>
 
 
 
-
+                                    <div class="mt-8 flex justify-center items-center ">
+                                        <button
+                                            class="px-4 flex justify-center items-center gap-2  py-3 font-extrabold rounded-3xl text-lg bg-cyan-500 text-white">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="3" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                            </svg>
+                                            Add new</button>
+                                    </div>
 
                                 </div>
 
@@ -327,7 +408,7 @@
             }).addTo(mymap0).bindPopup(
                 '<div class="flex flex-col justify-center text-xl font-bold text-center text-black rounded-xl"><p id="title" class="px-4 text-sm">' +
                 message + '</p></div><a href="' + url + '/' + placeid + '/' + placetype +
-                '" class="flex justify-center px-2 py-2 text-center bg-blue-600 rounded"><button class="text-white">' +
+                '" class="flex justify-center px-2 py-2 text-center bg-cyan-600 rounded"><button class="text-white">' +
                 readmore + '</button><a>'
             );
 
