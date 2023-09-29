1e9bbdce9b281ded6a4d98ad16f1fdacfd9a79b4
HeavyUmar <heavyumarfarooq@gmail.com>
2023-09-30 00:55:14 +0500

    make changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     @include('parts.head')
     @livewireStyles
     <link rel='stylesheet' href="https://unpkg.com/swiper@6.8.4/swiper-bundle.min.css">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </head>
 
